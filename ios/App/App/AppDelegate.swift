3d91086ca250aee049d2f61fba067bb83854cc2b
prueba <prueba@gmail.com>
2026-03-13 12:48:23 -0300

    FIX: Enable Push Notifications capability and add Email/WhatsApp to reservations

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Build 19 - Fixed Entitlements and Email Notifications
         FirebaseApp.configure()
         return ApplicationDelegateProxy.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
